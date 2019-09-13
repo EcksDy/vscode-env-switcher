e6f9690dd792f35b7156b8b704cebb370b390885
EcksDy <xackthesystem@gmail.com>
2019-09-13 17:18:42 +0300

    Constants updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,12 @@
-const extensionPrefix = "extension.";
+const EXTENSION_PREFIX = "extension.";
 
-export const labelPrefix = ".env: ";
+export const EXTENSION_NAME = ".ENV Switcher";
+export const LABEL_PREFIX = ".env: ";
+export const BUTTON_DEFAULT = "Select .env";
 
-export const fileHeaderStartToken = "#";
-export const fileHeaderEndToken = ".env";
+export const FILE_HEADER_START_TOKEN = "#";
+export const FILE_HEADER_END_TOKEN = ".env";
 
-export const selectEnvCommandId = `${extensionPrefix}selectEnv`;
+export const BACKUP_FILE_NAME = "switcher_backup";
+
+export const SELECT_ENV_COMMAND_ID = `${EXTENSION_PREFIX}selectEnv`;
