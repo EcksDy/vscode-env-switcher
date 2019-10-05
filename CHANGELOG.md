c73b6300c0985828ea3ab6f578df3201b8aa65ce
EcksDy <xackthesystem@gmail.com>
2019-10-05 23:48:57 +0300

    Changed activation event and added changelog.md

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Change Log
+
+Changes to **.ENV Switcher** can be found in this file.
+
+## - 0.1.2
+
+- Now using activation events - extension won't try to activate unless a workspace with `.env` file is found.
+
+- Added a CHANGELOG.md file.
+
+## - 0.1.1
+
+- Broken URI's are fixed for linux.
+
+- No longer showing pop-up warning message for missing headers, instead writting to log.
+
+## - 0.1.0
+
+- Initial release.
--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,9 @@ import { FileSystemHandler } from "./fsHandler";
 import { createStatusBar } from "./statusBar";
 
 export async function activate({ subscriptions }: vscode.ExtensionContext) {
-  // Don't activate if no workspace opened
-  if (vscode.workspace.workspaceFolders === undefined) {
-    return;
-  }
-
-  const fsHandler = new FileSystemHandler(vscode.workspace.workspaceFolders);
-  // Don't activate if workspace doesn't contain ".env" files
-  if ((await fsHandler.findCurrentEnvFile()).length === 0) {
-    return;
-  }
+  // Extension activation event will trigger only on a workspace with ".env" file in it,
+  // so we can assert that workspace folders are not undefined
+  const fsHandler = new FileSystemHandler(vscode.workspace.workspaceFolders!);
   fsHandler.backupEnvCurrentFile();
 
   const statusBar = await createStatusBar(fsHandler);
