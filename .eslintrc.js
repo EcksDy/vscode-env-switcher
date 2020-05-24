d5fae9e0dedd70f64009b7f8362fb368736943f7
EcksDy <xackthesystem@gmail.com>
2020-05-24 23:35:43 +0300

    Switched to eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript/base',
+    'airbnb-typescript-prettier',
+    'eslint:recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'no-console': 0,
+    'import/no-unresolved': 0,
+    'no-restricted-syntax': [
+      0,
+      {
+        selector: 'ForOfStatement',
+      },
+    ],
+  },
+};
