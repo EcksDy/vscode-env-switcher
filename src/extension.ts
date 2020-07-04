9f87fe3564182541bf7fe7dea0f1411330123c54
EcksDy <xackthesystem@gmail.com>
2020-07-04 15:48:37 +0300

    Added enabled/disabled config

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,25 @@
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, workspace } from 'vscode';
 import BackupViewerContentProvider from './content_providers/backupViewerContentProvider';
 import BackupHandler from './handlers/backupHandler';
 import CommandsHandler from './handlers/commandsHandler';
 import FileSystemHandler from './handlers/fsHandler';
 import EnvStatusBarItem from './status_bar_items/envStatusBarItem';
 import { selectedEnvPresetEventEmitter } from './utilities/events';
+import { EXTENSION_PREFIX } from './utilities/consts';
+
+/**
+ * Will get the extension `enabled` config from workspace settings, with global settings fallback.
+ */
+const extensionEnabled = () =>
+  workspace.getConfiguration(`${EXTENSION_PREFIX}`).get('enabled') as boolean;
 
 export async function activate({
   subscriptions,
   globalStoragePath,
   storagePath,
 }: ExtensionContext) {
+  if (!extensionEnabled()) return;
+
   // Making sure a workspace is opened so we can assert workspace related objects later
   if (storagePath === undefined) throw new Error('No workspace opened.');
 
