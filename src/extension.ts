149647b7988369c0070b9229298ce7a927750560
EcksDy <xackthesystem@gmail.com>
2021-04-12 17:48:30 +0300

    Returned extension activation where it belongs

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
-import { ExtensionContext, workspace } from 'vscode';
-import { initialize } from './config';
-import { EXTENSION_PREFIX } from './utilities/consts';
+import { ExtensionContext, workspace, commands } from 'vscode';
+import { SELECT_ENV_COMMAND_ID, EXTENSION_PREFIX } from './utilities/consts';
+import { selectEnvPreset } from './command_implementations/selectEnvPreset';
+import { FileSystemHandler, EnvHandler, EventHandlers } from './handlers';
+import EnvStatusBarButton from './ui_components/envStatusBarButton';
+import { PresetWatcher } from './watchers';
 
 /**
  * Will get the extension `enabled` config from workspace settings, with global settings fallback.
@@ -12,7 +15,43 @@ const extensionEnabled = workspace
 export async function activate({ subscriptions }: ExtensionContext) {
   if (!extensionEnabled) return;
 
-  await initialize({ subscriptions });
+  /* HANDLERS */
+  const fsHandler = new FileSystemHandler();
+  const envHandler = await EnvHandler.build({ fsHandler });
+
+  /* COMMANDS */
+  const selectEnvPresetCmd = commands.registerCommand(SELECT_ENV_COMMAND_ID, () =>
+    selectEnvPreset({
+      envHandler,
+      rootDir: fsHandler.rootDir.uri,
+    }),
+  );
+
+  /* UI COMPONENTS */
+  const statusBarButton = await EnvStatusBarButton.build({ envHandler });
+
+  /* WATCHERS */
+  const presetChangeWatcher = new PresetWatcher({
+    rootDir: fsHandler.rootDir,
+    envHandler,
+  });
+
+  /* EVENT HANDLERS */
+  const selectedEnvPresetEventHandler = EventHandlers.SelectedEnvPreset.register({
+    setter: statusBarButton,
+  });
+  const targetEnvChangedEventHandler = EventHandlers.TargetEnvChanged.register({
+    setter: statusBarButton,
+  });
+
+  /* REGISTRATION */
+  subscriptions.push(
+    selectedEnvPresetEventHandler,
+    targetEnvChangedEventHandler,
+    selectEnvPresetCmd,
+    statusBarButton,
+    presetChangeWatcher,
+  );
 }
 
 export function deactivate() {}
