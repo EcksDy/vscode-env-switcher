50152be58603325ad1f7845011d56c88c3eb00f5
Yury <ecksdy@gmail.com>
2023-08-11 16:53:36 +0300

    v0.4.3 (#40)
    * Bump lodash from 4.17.19 to 4.17.21
    
    Bumps [lodash](https://github.com/lodash/lodash) from 4.17.19 to 4.17.21.
    - [Release notes](https://github.com/lodash/lodash/releases)
    - [Commits](https://github.com/lodash/lodash/compare/4.17.19...4.17.21)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump hosted-git-info from 2.8.8 to 2.8.9
    
    Bumps [hosted-git-info](https://github.com/npm/hosted-git-info) from 2.8.8 to 2.8.9.
    - [Release notes](https://github.com/npm/hosted-git-info/releases)
    - [Changelog](https://github.com/npm/hosted-git-info/blob/v2.8.9/CHANGELOG.md)
    - [Commits](https://github.com/npm/hosted-git-info/compare/v2.8.8...v2.8.9)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump glob-parent from 5.1.1 to 5.1.2
    
    Bumps [glob-parent](https://github.com/gulpjs/glob-parent) from 5.1.1 to 5.1.2.
    - [Release notes](https://github.com/gulpjs/glob-parent/releases)
    - [Changelog](https://github.com/gulpjs/glob-parent/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/gulpjs/glob-parent/compare/v5.1.1...v5.1.2)
    
    ---
    updated-dependencies:
    - dependency-name: glob-parent
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Updated dependencies, bumped vscode to 1.71, cleaned up configs
    
    * More cleanup
    
    * Removed backup feature
    
    * Never liked camelCase for files anyway
    
    * Refactored with testing in mind (#28)
    
    * Updated readme (#29)
    
    * Fixed preset detection issue on startup (#30)
    
    * v0.4.0
    
    * Task/add precommit cleanup (#32)
    
    * Added a precommit hook to lint and format staged files
    
    * Changelog
    
    * v0.4.1
    
    * Windows paths were missing schema (#36)
    
    * Added release doc (#38)
    
    * Added release doc
    
    * Fixed formatting
    
    * Rephrased
    
    * Feat/overwrite alert (#39)
    
    * Added overwrite alert and fixed preset pick bug
    
    * Cleanup
    
    * Warning message instead of info
    
    * v0.4.3
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/command-implementations/select-env-preset.ts
+++ src/command-implementations/select-env-preset.ts
@@ -1,21 +1,26 @@
 import { QuickPickItem, window } from 'vscode';
 import { IButton, IPresetManager, Preset } from '../interfaces';
+import { ExtensionConfig } from '../utilities';
 
 const NO_FILE = 'No file';
 
+const OVERWRITE_ALERT_OPTIONS = [`OK`, `Don't show again`, `Cancel`] as const;
+
 interface Deps {
+  config: ExtensionConfig;
   presetManager: IPresetManager;
   button: IButton;
 }
 
-function presetToQuickPickItem({ description, title }: Preset): QuickPickItem {
+function presetToQuickPickItem({ description, title, path }: Preset): QuickPickItem {
   return {
     label: title,
     description,
+    detail: path,
   };
 }
 
-export async function selectEnvPreset({ presetManager, button }: Deps) {
+export async function selectEnvPreset({ config, presetManager, button }: Deps) {
   const presets = await presetManager.getPresets();
 
   const presetQuickPickList: QuickPickItem[] = presets.map(presetToQuickPickItem);
@@ -23,11 +28,18 @@ export async function selectEnvPreset({ presetManager, button }: Deps) {
   const selectedItem = await window.showQuickPick(presetQuickPickList);
   if (selectedItem === undefined) return;
 
-  const selectedPreset = presets.find(
-    ({ title, description }) =>
-      title === selectedItem.label && description === selectedItem.description,
-  );
+  const selectedPreset = presets.find(({ path }) => path === selectedItem.detail);
   if (!selectedPreset) return;
+
+  if (config.overwriteAlert.get()) {
+    const result = await window.showWarningMessage(
+      `You're about to overwrite your environment variables!\nPlease make sure you have a backup of your target file.`,
+      ...OVERWRITE_ALERT_OPTIONS,
+    );
+
+    if (result === 'Cancel') return;
+    if (result === `Don't show again`) await config.overwriteAlert.set(false);
+  }
 
   try {
     await presetManager.setCurrentPreset(selectedPreset);
