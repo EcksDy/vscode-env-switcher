01cafb100977a1751fb3b9cc4bf90bc13e9d441d
Yury <ecksdy@gmail.com>
2023-09-02 17:35:05 +0300

    Feature/extension testing framework (#44)
    * Configs and setup files
    
    * WIP extension testin framework
    
    * Regular repo sanity tests
    
    * Test on PR
    
    * Also dev
    
    * Typos
    
    * Cleanup
    
    * cis have no screens
    
    * Mb node?
    
    * Increase timeout for ci
    
    * Matrix test different OSes
    
    * Windows is going to be hard to test
    
    * Will wait for button to appear
    
    * Set version matrix
    
    * Formatting

---
+++ src/ui-test/regular-repo.test.ts
@@ -0,0 +1,73 @@
+import { expect } from 'chai';
+import {
+  InputBox,
+  StatusBar,
+  VSBrowser,
+  WebElement,
+  Workbench,
+  before,
+} from 'vscode-extension-tester';
+import {
+  Structures,
+  TEST_FILE_STRUCTURES,
+  generateTestingGrounds,
+  getExpectedPresetNames,
+  getStructureSettings,
+  resetTestingGrounds,
+} from './utilities/prepare-testing-grounds';
+import forEach from 'mocha-each';
+
+describe('regular repo setup', () => {
+  before(async () => {
+    await VSBrowser.instance.waitForWorkbench();
+  });
+
+  forEach(TEST_FILE_STRUCTURES).describe('%s structure', (structure: Structures) => {
+    let envSwitcherButton: WebElement;
+    let expectedPresetNames: string[];
+
+    before(async () => {
+      const testingGroundsPath = await generateTestingGrounds(structure);
+      expectedPresetNames = getExpectedPresetNames(structure);
+      const settings = getStructureSettings(structure);
+      if (settings) {
+        const settingsEditor = await new Workbench().openSettings();
+        for await (const [settingStr, value] of Object.entries(settings)) {
+          const { title, categories } = splitStringToTitleAndCategories(settingStr);
+          const setting = await settingsEditor.findSetting(title, ...categories);
+          await setting.setValue(value);
+        }
+      }
+
+      await VSBrowser.instance.openResources(testingGroundsPath);
+
+      const statusBar = new StatusBar();
+      envSwitcherButton = await VSBrowser.instance.driver.wait(async () => {
+        return (await statusBar.getItem('Select preset'))!;
+      }, 5_000);
+    });
+
+    after(async () => {
+      await resetTestingGrounds();
+    });
+
+    it('should suggest the expected presets', async () => {
+      await envSwitcherButton.click();
+
+      const presetSelect = await InputBox.create();
+      const availablePresets = await presetSelect.getQuickPicks();
+
+      const presetNames = await Promise.all(
+        availablePresets.map(async (preset) => (await preset.getLabel()).toLowerCase()),
+      );
+
+      expect(presetNames).to.have.members(expectedPresetNames);
+    });
+  });
+});
+
+function splitStringToTitleAndCategories(key: string): { title: string; categories: string[] } {
+  const [categoriesStr, title] = key.split(': ');
+  const categories = categoriesStr.split(' > ');
+  return { title, categories };
+}
